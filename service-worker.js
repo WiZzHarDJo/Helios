0fa3f7574bec9d618648faa8e42933da949d5913
Joaquim <jogi@itu.dk>
2020-06-15 14:09:20 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.78eaea1358c16f8b7a452015c21c2c1f.js"
+  "./precache-manifest.c5a5fe8bc0c8bf8d554cc68941b9db61.js"
 );
 
 self.addEventListener('message', (event) => {
